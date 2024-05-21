b4a8b92cafe4dbb8ad25cdab3ba143c47b586bff
MrCondeJV <lbarriosmunoz1012@gmail.com>
2024-05-21 16:01:33 -0500

    Nueva Dashboard Con Bootstrap 5

---
+++ login.php
@@ -0,0 +1,123 @@
+<?php
+session_start();
+require "./conexion.php";
+
+
+if ($_POST) {
+    $usuario = $_POST['usuario'];
+    $password = $_POST['password'];
+
+    $sql = "SELECT ID, Nombre, Usuario, contrasena, ID_Rol FROM usuarios WHERE Usuario = '$usuario' ";
+    $resultado = $mysqli->query($sql);
+    $num = $resultado->num_rows;
+
+    if ($num > 0) {
+        
+        $row = $resultado->fetch_assoc();
+        $password_bd = $row['contrasena'];
+        $pass_c = sha1($password);
+        
+
+        if ($password_bd == $pass_c) {
+
+            $_SESSION['id']=$row['ID'];
+            $_SESSION['nombre'] = $row['Nombre'];
+            $_SESSION['ID_Rol'] = $row['ID_Rol'];
+            header("Location: index.php");
+        } else {
+            echo "La contraseña es Incorrecta!!";
+        }
+    } else {
+        echo "No existe Usuario";
+    }
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Iniciar Sesión | ESFIM</title>
+
+    <!-- Fuentes personalizadas para esta plantilla -->
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+    <!-- Estilos personalizados para esta plantilla -->
+    <link href="css/sb-admin-2.min.css" rel="stylesheet">
+</head>
+
+<body class="bg-gradient-primary">
+    <div class="container">
+        <!-- Fila exterior -->
+        <div class="row justify-content-center">
+            <div class="col-xl-10 col-lg-12 col-md-9">
+                <div class="card o-hidden border-0 shadow-lg my-5">
+                    <div class="card-body p-0">
+                        <!-- Fila anidada dentro del cuerpo de la tarjeta -->
+                        <div class="row">
+                            <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
+                            <div class="col-lg-6">
+                                <div class="p-5">
+                                    <div class="text-center">
+                                        <h1 class="h4 text-gray-900 mb-4">¡Bienvenido de nuevo!</h1>
+                                    </div>
+                                    <form class="user" method="POST" action="<?php echo $_SERVER['PHP_SELF'];?>">
+                                        <div class="form-group">
+                                            <input type="text" class="form-control form-control-user" id="exampleInputEmail" name="usuario" aria-describedby="emailHelp" placeholder="Usuario">
+                                        </div>
+                                        <div class="form-group">
+                                            <input type="password" class="form-control form-control-user" id="exampleInputPassword" name="password" placeholder="Contraseña">
+                                        </div>
+                                        <div class="form-group">
+                                            <div class="custom-control custom-checkbox small">
+                                                <input type="checkbox" class="custom-control-input" id="customCheck">
+                                                <label class="custom-control-label" for="customCheck">Recordarme</label>
+                                            </div>
+                                        </div>
+                                        <button type="submit" class="btn btn-primary btn-user btn-block">
+                                            Iniciar Sesión
+                                        </button>
+                                        <hr>
+                                        <a href="index.html" class="btn btn-google btn-user btn-block">
+                                            <i class="fab fa-google fa-fw"></i> Iniciar con Google
+                                        </a>
+                                        <a href="index.html" class="btn btn-facebook btn-user btn-block">
+                                            <i class="fab fa-facebook-f fa-fw"></i> Iniciar con Facebook
+                                        </a>
+                                    </form>
+                                    <hr>
+                                    <div class="text-center">
+                                        <a class="small" href="forgot-password.html">¿Olvidaste tu contraseña?</a>
+                                    </div>
+                                    <div class="text-center">
+                                        <a class="small" href="register.html">¡Crear una cuenta!</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Núcleo de JavaScript de Bootstrap -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Complemento de JavaScript de Easing -->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Scripts personalizados para todas las páginas -->
+    <script src="js/sb-admin-2.min.js"></script>
+</body>
+
+</html>
