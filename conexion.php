eb272fdf5e054befede35b4128141bace6d53278
MrCondeJV <lbarriosmunoz1012@gmail.com>
2024-05-31 13:46:14 -0500

    Cerrar sesion actualizado

--- conexion.php
+++ conexion.php
@@ -2,6 +2,6 @@
 $mysqli = new mysqli('15.235.86.58', 'esfimedu_luis', 'k%-eDD4n3xDz', 'esfimedu_db_das_esfim');
 
 if ($mysqli->connect_error) {
-    die("Error de conexión: " . $mysqli->connect_error);
+    die("Error de conexion: " . $mysqli->connect_error);
 }
 ?>
